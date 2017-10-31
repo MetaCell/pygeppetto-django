219ee3f553f8164cd50a540db23dcaf44547280a
Adrian Quintana <adrianquintana@gmail.com>
2017-10-31 13:12:10 +0000

    first version

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='pygeppetto-django',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Python Geppetto Core Server developed in Django',
+    long_description=README,
+    url='https://github.com/MetaCell/pygeppetto-django',
+    author="MetaCell",
+    author_email="info@metacell.us",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
