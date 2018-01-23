5d9abb9876f5b3e29afecba77a9bb0e6ec9f4d73
NeZanyat <rlatyshenko.dev@gmail.com>
2018-01-23 17:04:10 +0200

    Required for deployment to be working

--- pygeppetto_server/routing.py
+++ pygeppetto_server/routing.py
@@ -1,4 +1,4 @@
-from consumers import ws_connect, ws_receive, ws_disconnect
+from pygeppetto_server.consumers import ws_connect, ws_receive, ws_disconnect
 from channels import route
 
 server_routing = [
